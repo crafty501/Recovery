8b5d6d3b46ab49e2f97938620a8a7ac5d413fdfd
n1kl <nick4542@yahoo.com>
2016-06-05 02:59:16 +0200

    formatiert

--- src/persistence_manager/IManager.java
+++ src/persistence_manager/IManager.java
@@ -2,9 +2,10 @@ package persistence_manager;
 
 public interface IManager {
 
-	
-	public  int beginTransaction();
+	public int beginTransaction();
+
 	public void commit(int taid);
+
 	public void write(int taid, int pageid, String data);
 
 }
